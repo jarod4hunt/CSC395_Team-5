7a26691463185b01f2ce5b11ca9c6c887cf55127
jarod4hunt <jarod4hunt@gmail.com>
2024-09-17 18:46:18 -0400

    more test work 9/17

--- flask-app/app/app.py
+++ flask-app/app/app.py
@@ -1,48 +1,38 @@
-# from flask import Flask, render_template, request, abort, Response
-# app = Flask(__name__)
-
-# # Basic Authentication
-# def check_auth(username, password):
-#     return username == 'csc395' and password == 'team5'
-
-# def authenticate():
-#     return Response(
-#         'Could not verify your access level for that URL.\n'
-#         'You have to login with proper credentials', 401,
-#         {'WWW-Authenticate': 'Basic realm="Login Required"'})
-
-# def requires_auth(f):
-#     def decorated(*args, **kwargs):
-#         auth = request.authorization
-#         if not auth or not check_auth(auth.username, auth.password):
-#             return authenticate()
-#         return f(*args, **kwargs)
-#     return decorated
-
-# @app.route('/')
-# @requires_auth
-# def index():
-#     return render_template('delvec_index.html')
-
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0', port=5000, debug=True)
-
-
-from flask import Flask, request, render_template
-
+from flask import Flask, render_template, request, abort, Response
 app = Flask(__name__)
 
-@app.route('/')
+# Basic Authentication
+def check_auth(username, password):
+    return username == 'csc395' and password == 'team5'
+
+def authenticate():
+    return Response(
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+@app.route('/', methods=['GET', 'POST'])
+@requires_auth
 def index():
-    return render_template('test.html')
-
-@app.route('/submit', methods=['POST'])
-def submit():
-    # Capture input from the form
-    name = request.form['name']
+    name = None
+    greeting = None
     
-    # Process the input and display a response
-    return f'Hello, {name}!'
+    if request.method == 'POST':
+        name = request.form['name']
+        greeting = f'Hello, {name}!'
+    
+    return render_template('index.html', name=name, greeting=greeting)
+   
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
+
+
