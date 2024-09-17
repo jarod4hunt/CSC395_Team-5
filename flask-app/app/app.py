efd5f2d0c50a6b9e905ae947d324778d8bc8b6dd
jarod4hunt <jarod4hunt@gmail.com>
2024-09-17 18:36:53 -0400

    testing taking web input into python

--- flask-app/app/app.py
+++ flask-app/app/app.py
@@ -1,13 +1,48 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template, request, abort, Response
+# app = Flask(__name__)
+
+# # Basic Authentication
+# def check_auth(username, password):
+#     return username == 'csc395' and password == 'team5'
+
+# def authenticate():
+#     return Response(
+#         'Could not verify your access level for that URL.\n'
+#         'You have to login with proper credentials', 401,
+#         {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+# def requires_auth(f):
+#     def decorated(*args, **kwargs):
+#         auth = request.authorization
+#         if not auth or not check_auth(auth.username, auth.password):
+#             return authenticate()
+#         return f(*args, **kwargs)
+#     return decorated
+
+# @app.route('/')
+# @requires_auth
+# def index():
+#     return render_template('delvec_index.html')
+
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=5000, debug=True)
+
+
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    name = "User"  # This can be dynamically set
-    return render_template('calc.html')
+    return render_template('test.html')
 
-if __name__ == "__main__":
+@app.route('/submit', methods=['POST'])
+def submit():
+    # Capture input from the form
+    name = request.form['name']
+    
+    # Process the input and display a response
+    return f'Hello, {name}!'
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
-
