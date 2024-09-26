3f908338a96377ca5c18cbdfb1abf0bf8a2fdfda
jarod4hunt <jarod4hunt@gmail.com>
2024-09-26 09:03:12 -0400

    have to rename src-ollama with directory change

--- src/startup_script.sh
+++ src/startup_script.sh
@@ -6,8 +6,8 @@ docker-compose up --build -d
 # Wait for a few seconds to ensure the Ollama container is up
 sleep 1  # Adjust this time based on how long it takes for your container to start
 
-docker exec -it csc395_team-5-ollama-1 ollama serve 
+docker exec -it src-ollama-1 ollama serve 
 sleep 5 
 
 # Execute the Ollama command
-docker exec -it csc395_team-5-ollama-1 ollama run llama3  # Change the container name if needed
+docker exec -it src-ollama-1 ollama run llama3  # Change the container name if needed
