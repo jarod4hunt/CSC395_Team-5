2d9f1693e9cfc37671aa2af65f4fc9e69a4f12df
jarod4hunt <124277765+jarod4hunt@users.noreply.github.com>
2024-09-27 10:51:10 -0400

    Update startup_script.sh

--- src/startup_script.sh
+++ src/startup_script.sh
@@ -4,9 +4,6 @@
 docker-compose up --build -d
 
 # Wait for a few seconds to ensure the Ollama container is up
-sleep 1  # Adjust this time based on how long it takes for your container to start
-
-docker exec -it src-ollama-1 ollama serve 
 sleep 5 
 
 # Execute the Ollama command
