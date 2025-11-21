a6d555ce8464a3dc393813b7ddf4799d0ec54c13
Jarod Hunt <jarod4hunt@gmail.com>
2025-11-21 11:08:14 -0500

    Updated for new Docker Compose

--- src/startup_script.sh
+++ src/startup_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Start the Docker containers in detached mode
-docker-compose up --build -d
+docker compose up --build -d
 
 # Wait for a few seconds to ensure the Ollama container is up
-sleep 5 
+sleep 10 
 
 # Execute the Ollama command
 docker exec -it src-ollama-1 ollama run llama3  # Change the container name if needed
